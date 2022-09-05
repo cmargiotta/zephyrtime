99ef249f909de78f50e3e850c98422151a1fc0f1
Carmine Margiotta <cmargiotta@posteo.net>
2022-09-05 22:56:26 +0200

    Added minimal development environment

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils/master";
+    devshell.url = "github:numtide/devshell/master";
+  };
+  outputs = { self, nixpkgs, flake-utils, devshell }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ devshell.overlay ];
+          config.allowUnfree = true;
+        };
+
+      in
+      rec {
+        devShell = pkgs.devshell.mkShell {
+          name = "zephyr-pinetime";
+          bash = {
+            extra = ''
+              export LD_INCLUDE_PATH="$DEVSHELL_DIR/include"
+              export LD_LIB_PATH="$DEVSHELL_DIR/lib"
+              west init 2> /dev/null
+            '';
+            interactive = "";
+          };
+          commands = [
+          ];
+          env = [
+          ];
+          packages = [
+            pkgs.python39Packages.west
+          ];
+        };
+      });
+}
